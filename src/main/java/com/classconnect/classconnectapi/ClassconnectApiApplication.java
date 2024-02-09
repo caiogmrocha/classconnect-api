1f51d300b86f32887e2f62c1e0024df4a29b92f5
caiogmrocha <caioguilherme739@gmail.com>
2024-02-08 21:53:51 -0300

    initial commit

---
+++ src/main/java/com/classconnect/classconnectapi/ClassconnectApiApplication.java
@@ -0,0 +1,13 @@
+package com.classconnect.classconnectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassconnectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassconnectApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/classconnect/classconnectapi/ClassconnectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classconnect.classconnectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassconnectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
