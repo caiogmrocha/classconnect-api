d73289edefa58c9a2b0d9ef56a8a7920178156fc
caiogmrocha <caioguilherme739@gmail.com>
2024-02-25 15:55:00 -0300

    feat: configurar CORS

---
+++ src/main/java/com/classconnect/classconnectapi/configuracao/HttpWebConfiguration.java
@@ -0,0 +1,14 @@
+package com.classconnect.classconnectapi.configuracao;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class HttpWebConfiguration implements WebMvcConfigurer {
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
+  }
+}
