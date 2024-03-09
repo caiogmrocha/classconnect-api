2cb03d1069652f66f2fb46de691c443a32adfbbe
caiogmrocha <caioguilherme739@gmail.com>
2024-03-08 22:54:40 -0300

    feat(CC-91): criar o endpoint PATCH /api/salas/{idSala}/matriculas/aceitar/{idAluno}

--- src/main/java/com/classconnect/classconnectapi/comunicacao/MatriculaController.java
+++ src/main/java/com/classconnect/classconnectapi/comunicacao/MatriculaController.java
@@ -15,6 +15,10 @@ import com.classconnect.classconnectapi.negocio.servicos.MatriculasService;
 import com.classconnect.classconnectapi.negocio.servicos.excecoes.AlunoJaMatriculadoSalaException;
 import com.classconnect.classconnectapi.negocio.servicos.excecoes.AlunoNaoExisteException;
 import com.classconnect.classconnectapi.negocio.servicos.excecoes.SalaNaoExisteException;
+import com.classconnect.classconnectapi.negocio.servicos.excecoes.SalaNaoPertenceProfessorException;
+import com.classconnect.classconnectapi.negocio.servicos.excecoes.SolicitacaoMatriculaNaoExiste;
+
+import org.springframework.web.bind.annotation.PatchMapping;
 
 @RestController
 @RequestMapping("/api/salas/{idSala}/matriculas")
@@ -36,6 +40,30 @@ public class MatriculaController {
         return ResponseEntity.badRequest().body(Map.of("mensagem", "Sala não existe"));
       } catch (AlunoNaoExisteException e) {
         return ResponseEntity.badRequest().body(Map.of("mensagem", "Aluno não existe"));
+      } catch (Exception e) {
+        e.printStackTrace();
+        return ResponseEntity.badRequest().body(Map.of("mensagem", "Internal server error"));
+      }
+    }
+
+    @PatchMapping("/aceitar/{idAluno}")
+    public ResponseEntity<Map<?,?>> professorAceitarSolicitacao(@PathVariable Long idSala, @PathVariable Long idAluno) {
+      var perfil = (Perfil) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+      try {
+        this.matriculaService.professorAceitarSolicitacao(idSala, idAluno, perfil.getId());
+
+        return ResponseEntity.ok(Map.of("mensagem", "Solicitação de matrícula aceita"));
+      } catch (AlunoJaMatriculadoSalaException e) {
+        return ResponseEntity.badRequest().body(Map.of("mensagem", "Aluno já matriculado na sala"));
+      } catch (SalaNaoExisteException e) {
+        return ResponseEntity.badRequest().body(Map.of("mensagem", "Sala não existe"));
+      } catch (AlunoNaoExisteException e) {
+        return ResponseEntity.badRequest().body(Map.of("mensagem", "Aluno não existe"));
+      } catch (SalaNaoPertenceProfessorException e) {
+        return ResponseEntity.badRequest().body(Map.of("mensagem", "Sala não pertence ao professor"));
+      } catch (SolicitacaoMatriculaNaoExiste e) {
+        return ResponseEntity.badRequest().body(Map.of("mensagem", "Solicitação de matrícula não existe"));
       } catch (Exception e) {
         e.printStackTrace();
         return ResponseEntity.badRequest().body(Map.of("mensagem", "Internal server error"));
